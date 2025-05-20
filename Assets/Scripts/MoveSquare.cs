fce022186b5c0a788fc0852a836edb97857ae676
hebiii994 <salvatoreguglielmini964@gmail.com>
2025-05-20 10:25:22 +0200

    fix

--- Assets/Scripts/MoveSquare.cs
+++ Assets/Scripts/MoveSquare.cs
@@ -7,8 +7,9 @@ public class MoveSquare : MonoBehaviour
 
     [SerializeField] private float _velocita = 5;
     [SerializeField] private Rigidbody2D _rb;
-    [SerializeField] private Vector2 maxDistance = new Vector2(11, 6);
-    [SerializeField] private Vector2 startPosition = new Vector2(0, 0);
+    // [SerializeField] private Vector2 maxDistance = new Vector2(11, 6);
+    [SerializeField] private float maxRadius = 10f; // Esempio: raggio di 10 unità
+    [SerializeField] private Vector2 startPosition;
 
 
 
@@ -19,38 +20,31 @@ public class MoveSquare : MonoBehaviour
     {
         
         _rb = GetComponent<Rigidbody2D>();
-        _rb.position = startPosition;
-        
-
-      
+        startPosition = _rb.position;
 
     }
 
     private void Update()
     {
         Debug.Log(_rb.position);
-
-        if (_rb.position.x >= maxDistance.x || _rb.position.y >= maxDistance.y)
-        {
-            _rb.position = startPosition;
-
-        }
-        else if (_rb.position.x <= -maxDistance.x || _rb.position.y <= -maxDistance.y)
-        {
-            _rb.position = startPosition;
-        }
     }
 
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
         _rb.MovePosition(_rb.position + direction * (_velocita * Time.deltaTime));
-        
+
+        float currentDistance = Vector2.Distance(_rb.position, startPosition);
+        if (currentDistance > maxRadius)
+        {
+            _rb.position = startPosition;
+        }
 
 
-    }
+        }
 
 
 
